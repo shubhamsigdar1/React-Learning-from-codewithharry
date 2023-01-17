2a8bcdfbac34ec26738043f115504157e798d2f5
shubhamsigdar1 <shubhamsigdar@gmail.com>
2023-01-18 01:25:15 +0530

    class 9 task complted

---
+++ module1.mjs
@@ -0,0 +1,3 @@
+import dza, {a} from './module2.mjs'
+console.log(dza);
+console.log(a)
---
+++ src/components/TextForm.js
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        let newText = text.toUpperCase();
+        setText(newText)
+    }
+    const handleLowClick = () => {
+        let newText = text.toLowerCase();
+        setText(newText)
+    }
+    const handleCLearClick = () => {
+        let newText = ""
+        setText(newText)
+    }
+    const handleCapClick =()=>{
+        let newText=text.charAt(0).toUpperCase() + text.slice(1)
+        setText(newText)
+    }
+    const handleCopyClick=()=>{
+        let copyText= document.querySelector('#myBox')
+        navigator.clipboard.writeText(copyText.value);
+        let newText=copyText.value;
+        console.log(newText)
+        setText(newText)
+    }
+    const handleExtraSpace =()=>{
+        let newText=text.replace(/\s+/g, ' ').trim()
+        setText(newText)
+    }
+    const handleOnChange = (event) => {
+        setText(event.target.value)
+    }
+
+    const [text, setText] = useState("")
+    return (
+        <>
+            <div className='container'>
+                <div className="mb-3">
+                    <label htmlfor="myBox" className="form-label">{props.heading}</label>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8" placeholder='enter a text'></textarea>
+                </div>
+                <div class="mb-3">
+                    <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
+                    <button className='btn btn-primary mx-2' onClick={handleLowClick}>Convert to Lowercase</button>
+                    <button className='btn btn-primary' onClick={handleCLearClick}>Clear the text</button>
+                    <button className='btn btn-primary' onClick={handleCapClick}>Capitalize the text</button>
+                    
+                    <button className='btn btn-primary' onClick={handleCopyClick}>Copy to clickboard</button>
+                    <button className='btn btn-primary' onClick={handleExtraSpace}>Remove extra space</button>
+                </div>
+
+            </div>
+            <div className='container'>
+                <h1>Your Text Summary</h1>
+                <p>{text.split(" ").length} words {text.length} characters </p>
+                <p>{0.008 * text.split(" ").length} minutes read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        </>
+    )
+}
